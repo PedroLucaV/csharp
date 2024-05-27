b1434fca24cc7377138c4698f1df08e1527e1961
Pedro <izielucas2016@gmail.com>
2024-05-26 21:55:28 -0300

    criada e finalizada atividade1 com console

---
+++ Atividade1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.00;
+            double preco2 = 650.50;
+            double media = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é {preco1}");
+            Console.WriteLine($"{produto2}, cujo preço é {preco2}");
+            Console.WriteLine("");
+            Console.WriteLine($"Registro: {idade} anos de idade, código: {codigo} e gênero: {genero}");
+            Console.WriteLine($"Medida com 8 casas decimais: {media:F8}");
+            Console.WriteLine($"Arredondado (3 Casas): {media:F3}");
+            Console.WriteLine($"Separador decimal invariant culture: {media.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
