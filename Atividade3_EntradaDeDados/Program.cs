55f19c5dca8c6fb76ada92c6ab97abaf3f7b6cb7
Pedro <izielucas2016@gmail.com>
2024-05-27 23:23:24 -0300

    atividade com entrada de dados

---
+++ Atividade3_EntradaDeDados/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade3_EntradaDeDados
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual seu nome?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos você tem em casa?");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o preço do produto você quer comprar?");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Agora diga seu ultimo nome, sua idade e sua altura (mesma linha separado por espaço)");
+            string[] vet = Console.ReadLine().Split(" ");
+
+            string ultimoNome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            Console.WriteLine($"{nome}, {quartos}, {preco.ToString("F2", CultureInfo.InvariantCulture)}, {ultimoNome}, {idade} e {altura.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
