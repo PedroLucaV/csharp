2b08ba60d18dfa33442e4a7d84a8c513edfb792a
Pedro <izielucas2016@gmail.com>
2024-05-27 23:23:42 -0300

    atividade com baskara e entrada de dados

---
+++ Atividade4_Baskhara_Com_Entrada/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade4_Baskhara_Com_Entrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insira o valor de A");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o valor de B");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o valor de C");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+    }
+}
