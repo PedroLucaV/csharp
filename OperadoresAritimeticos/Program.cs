e2ee90b7cfb69117fb505de67055e705e616fac6
Pedro <izielucas2016@gmail.com>
2024-05-27 23:25:04 -0300

    operadores artitimeticos criado

---
+++ OperadoresAritimeticos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OperadoresAritimeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3 + 4 * 2;
+            int num2 = (3 + 4) * 2;
+            int num3 = 17 % 3;
+            double num4 = (double) 10 / 8;
+
+            Console.WriteLine(num4);
+        }
+    }
+}
