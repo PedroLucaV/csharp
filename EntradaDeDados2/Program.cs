e0791fb990f297e08d91249082ec7420c0b6bf66
Pedro <izielucas2016@gmail.com>
2024-05-27 23:24:50 -0300

    entrada de dados 2

---
+++ EntradaDeDados2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EntradaDeDados2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine()); //Converte o valor do ReadLine para int assim evitando o problema
+            Console.WriteLine(x);
+        }
+    }
+}
