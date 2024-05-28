8c3c012b93d9ce2175096e6d59780900ccac3926
Pedro <izielucas2016@gmail.com>
2024-05-27 23:23:04 -0300

    atividade com formula de baskhara

---
+++ Atividade2_Baskhara/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atividade2_Baskhara {
+    class Program
+    {
+        static void Main(string[] args) { 
+            double a = 12, b = 12, c = 12;
+            double delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+    }
+}
